743658ffcea39db22b6bc3491d6526e4862845b1
Keyneto <Brize2302@yandex.ru>
2020-10-26 15:53:48 +0500

    Commit one

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,23 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanOne =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanOne.getMessage());
+
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+
+        Cat beanTree =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(beanTree.getMessage());
+
+        Cat beanFour =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(beanFour.getMessage());
+
+        System.out.println(beanOne.equals(beanTwo));
+        System.out.println(beanTree.equals(beanFour));
     }
 }
